147a4e331e0e7e9132f7f68f416eae87b93c89b9
davidmaya79 <davidmaya79@gmail.com>
2023-05-21 15:41:35 -0400

    migracion auditoria

---
+++ database/migrations/2023_05_21_191452_create_auditorias_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auditorias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->string('descripcion');
+            $table->string('documentacion');
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auditorias');
+    }
+};
